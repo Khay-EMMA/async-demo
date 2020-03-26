38a981bd1b9a4e70e42ddbe1191164e65fd552f9
njokuScript <kalunjoku123@gmail.com>
2020-03-26 12:34:22 +0100

    added callback function

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ console.log("After");
 const getUser = (id, callBack) => {
   setTimeout(() => {
     console.log("Reading user from the database");
-    callBack({ id: id, gitHubUserName: "mosh" });
-  }, 3000);
+    callBack({ id: id, gitHubUserName: "Njoku" });
+  }, 2000);
 };
 
 getUser(1, user => {
-  console.log("user", user);
+  getRepos(user.gitHubUserName, repo => {
+    console.log("repo", repo);
+  });
 });
+
+const getRepos = (username, callBack) => {
+  setTimeout(
+    () => {
+      callBack(["repo1", "repo2", "repo3"]);
+    },
+
+    2000
+  );
+};
